c6a3c20e7b736f88fe7c449a83b69df26124739d
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:27:20 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,13 @@
+package com.Method;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+	    double d = 1.0e3;
+	    float f = 0123;
+	    System.out.println(f);
+	    System.out.println(d);
+
+	}
+
+}
