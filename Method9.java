6cf94116a7cad4c19a874081a4cac09d79cd0658
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:24:31 +0530

    Add files via upload

---
+++ Method9.java
@@ -0,0 +1,17 @@
+package com.Method;
+
+public class Method9 {
+
+	public static void main(String[] args) {
+		Method9 m =new Method9();
+		
+    System.out.println("sum of total value : "+m.sum(40,30));
+		
+	}
+
+	
+	 int  sum (int a , int b) {
+		int sum = a+b;
+		return sum;
+	}
+}
