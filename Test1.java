2efee3c0206f1a0c2c55b8a45a245b2c87ae5379
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:27:02 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,17 @@
+package com.Method;
+
+public class Test1 {
+	int a;
+	int b;
+	public static void main (String[]args) {
+		Test1 t = new Test1();
+		
+		t. a = 80;
+		 t.b = 34;
+		Test1 t1 = new Test1();
+		System.out.println(t1.a);
+		System.out.println(t1.b);
+		
+	}
+
+}
